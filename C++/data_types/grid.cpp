213cee0c442bed9c671dd2a3a3e18b4e7d8ff148
aevalenc <aevalenc@syr.edu>
2022-10-16 21:00:39 +0200

    Add cpp and test files for grid class

---
+++ C++/data_types/grid.cpp
@@ -0,0 +1,7 @@
+///
+/// Author: Alejandro Valencia
+/// Boston University CFD Course: grid class
+/// Update: 16 October, 2022
+///
+
+#include "C++/data_types/grid.h"
---
+++ C++/data_types/test/grid_test.cpp
@@ -0,0 +1,35 @@
+// Alejandro Valencia
+// CFD BU Online Course Step 1: 1D conduction
+// Update: 20 November, 2021
+
+/************************************************************************
+ * This program uses a finite difference method to solve the 1D
+ ** diffusion equation as Step 3 of 12 of the online BU CFD course * taught by
+ *Lorena Barba
+ **
+ ************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "C++/data_types/spatial_variable.h"
+
+namespace cfd
+{
+namespace geometry
+{
+namespace
+{
+
+TEST(BaseClassTests,
+     GivenDefaultInstantiation_ExpectDefaultSpatialDiscretizationMethod)
+{
+    const SpatialVariable u;
+    const auto result = u.GetSpatialDiscretizationMethod();
+    EXPECT_TRUE(result == SpatialDiscretizationMethod::kFiniteDifferenceMethod);
+}
+
+}  // namespace
+
+}  // namespace geometry
+
+}  // namespace cfd
