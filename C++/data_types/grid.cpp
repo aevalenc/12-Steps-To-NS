4650e5997de30b0c497615eb3003e9596a875c6c
aevalenc <aevalenc@syr.edu>
2022-10-17 18:42:11 +0200

    Adjust grid data types

--- C++/data_types/grid.cpp
+++ C++/data_types/grid.cpp
@@ -5,3 +5,15 @@
 ///
 
 #include "C++/data_types/grid.h"
+
+namespace cfd
+{
+
+namespace geometry
+{
+
+std::uint64_t Grid::GetSize() const { return elements_.size(); }
+
+}  // namespace geometry
+
+}  // namespace cfd
