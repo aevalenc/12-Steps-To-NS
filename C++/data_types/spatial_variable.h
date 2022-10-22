6a075e8f643e637d0dfe3cb1678818d7f24041ba
aevalenc <aevalenc@syr.edu>
2022-10-22 17:33:53 +0200

    Adjust spatial variable to initial time variable

--- C++/data_types/spatial_variable.h
+++ C++/data_types/spatial_variable.h
@@ -6,9 +6,8 @@
  *
  */
 
-#include <vector>
-
 #include "C++/data_types/grid.h"
+#include <vector>
 
 #ifndef CXX_DATA_TYPES_SPATIAL_VARIABLE_H
 #define CXX_DATA_TYPES_SPATIAL_VARIABLE_H
@@ -27,34 +26,28 @@ enum class SpatialDiscretizationMethod
 class SpatialVariable
 {
   public:
-    SpatialVariable()
-        : spatial_discretization_method_{SpatialDiscretizationMethod::kInvalid}
-    {
-    }
+    SpatialVariable() : spatial_discretization_method_{SpatialDiscretizationMethod::kInvalid} {}
 
     ~SpatialVariable() {}
     SpatialVariable(const SpatialVariable& other)
         : spatial_discretization_method_(other.spatial_discretization_method_),
-          dummy_vector_(other.dummy_vector_){};
+          discretized_variable_(other.discretized_variable_){};
     SpatialVariable(SpatialVariable&& other)
         : spatial_discretization_method_(other.spatial_discretization_method_),
-          dummy_vector_(other.dummy_vector_){};
-    SpatialVariable& operator=(const SpatialVariable& other)
-    {
-        return *this = SpatialVariable(other);
-    }
+          discretized_variable_(other.discretized_variable_){};
+    SpatialVariable& operator=(const SpatialVariable& other) { return *this = SpatialVariable(other); }
     SpatialVariable& operator=(SpatialVariable&&) { return *this; }
 
-    void SetSpatialDiscretizationMethod(
-        SpatialDiscretizationMethod spatial_discretization_method);
+    void SetSpatialDiscretizationMethod(SpatialDiscretizationMethod spatial_discretization_method);
     SpatialDiscretizationMethod GetSpatialDiscretizationMethod() const;
+    std::vector<double>& GetDiscretizedVariable() { return discretized_variable_; };
 
     void SetGrid(const cfd::geometry::Grid& grid);
 
   private:
     SpatialDiscretizationMethod spatial_discretization_method_;
-    std::vector<double> dummy_vector_{};
-    cfd::geometry::Grid grid_;
+    std::vector<double> discretized_variable_{};
+    geometry::Grid spatial_grid_{};
 };
 
 }  // namespace cfd
