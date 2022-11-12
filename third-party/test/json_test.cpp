02f8cd90d480bafdd9ee1d915844fcd0256ea297
aevalenc <aevalenc@syr.edu>
2022-11-12 18:52:06 +0100

    Introduce third-party dependency folder
    First bite: introduce dependency on nlohmann_json

---
+++ third-party/test/json_test.cpp
@@ -0,0 +1,19 @@
+///
+/// Author: Alejandro Valencia
+/// Update: 12 November, 2022
+///
+
+#include <gtest/gtest.h>
+
+#include <nlohmann/json.hpp>
+
+namespace
+{
+TEST(JSON_TESTS, TestImportOfThirdPartyRepo)
+{
+    // nlohmann::json::
+    bool result{true};
+    EXPECT_TRUE(result);
+}
+
+}  // namespace
