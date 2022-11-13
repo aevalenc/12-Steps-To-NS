93d958477a22c2c965b78d028f744f439da7de40
aevalenc <aevalenc@syr.edu>
2022-11-13 10:16:58 +0100

    Added unit tests for importing jsons

--- third-party/test/json_test.cpp
+++ third-party/test/json_test.cpp
@@ -1,19 +1,42 @@
 ///
 /// Author: Alejandro Valencia
-/// Update: 12 November, 2022
+/// Update: 13 November, 2022
 ///
 
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <nlohmann/json.hpp>
+#include <optional>
 
 namespace
 {
-TEST(JSON_TESTS, TestImportOfThirdPartyRepo)
+
+TEST(JsonTestBaseFixture, TestImportOfThirdPartyRepo)
 {
-    // nlohmann::json::
-    bool result{true};
-    EXPECT_TRUE(result);
+    // Given
+    std::optional<std::ifstream> input_file_(
+        "third-party/test/json_test_input.json");
+    const std::string name("json_test_input");
+
+    // Assert
+    ASSERT_TRUE(input_file_.has_value());
+
+    // When
+    const auto input_data = nlohmann::json::parse(input_file_.value());
+
+    // Expect
+    EXPECT_TRUE(!input_data.empty());
+
+    EXPECT_TRUE(input_data["name"] == name);
+
+    EXPECT_TRUE(input_data["plain_object"]["pi"] == 3.14);
+
+    EXPECT_TRUE(!input_data["vector_object"].empty());
+    EXPECT_NEAR(input_data["vector_object"]["data"][0], 1, 0.01);
+
+    EXPECT_TRUE(!input_data["plain_vector"].empty());
+    EXPECT_NEAR(input_data["plain_vector"][2], 3, 0.01);
 }
 
 }  // namespace
