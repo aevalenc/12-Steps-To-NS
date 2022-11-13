2ce001383edfb26be02355f862964eb05150cbf2
aevalenc <aevalenc@syr.edu>
2022-11-13 21:00:40 +0100

    Initialize spatial and time variables

--- C++/src/step_1.cpp
+++ C++/src/step_1.cpp
@@ -19,20 +19,29 @@ int main(int argc, char *argv[])
 
     // Setup grid
     const auto grid_inputs = input_data["grid"];
+    cfd::geometry::GridGenerator grid_generator{};
     if (!grid_inputs["import"])
     {
-        cfd::geometry::GridGenerator grid_generator{};
         grid_generator.Create1DLinearGrid(
             grid_inputs["size"], grid_inputs["x_start"], grid_inputs["x_end"]);
     }
+    std::cout << "Grid generated successfully\n";
 
     // Initialize spatial variable
     const auto spatial_inputs = input_data["spatial_inputs"];
     cfd::SpatialVariable u_x{};
 
     u_x.SetSpatialDiscretizationMethod(spatial_inputs["discretization_method"]);
+    u_x.SetGrid(grid_generator.GetGrid());
+    std::cout << "Spatial variable initialized with grid\n";
+
+    // Initialize time variable
+    const auto time_inputs = input_data["time_inputs"];
+    cfd::TimeVariable u_t(u_x);
+    u_t.SetTimeDiscretizationMethod(time_inputs["discretization_method"]);
+    std::cout << "Time variable constructed with spatial variable\n";
 
     // Exit
-    std::cout << "End of program\n";
+    std::cout << "End of program" << std::endl;
     return 0;
 }
