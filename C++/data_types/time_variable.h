dfc137c7fbf95e01e2c4b0ae3b23f10dfe9d46b2
aevalenc <aevalenc@syr.edu>
2023-03-12 11:41:05 +0100

    TV: Fix compile issues

--- C++/data_types/time_variable.h
+++ C++/data_types/time_variable.h
@@ -6,9 +6,8 @@
  *
  */
 
-#include <vector>
-
 #include "C++/data_types/spatial_variable.h"
+#include <vector>
 
 #ifndef CXX_DATA_TYPES_TIME_VARIABLE_H
 #define CXX_DATA_TYPES_TIME_VARIABLE_H
@@ -18,7 +17,7 @@ namespace cfd
 
 enum class TimeDiscretizationMethod
 {
-    kEulerStep,
+    kEulerStep = 0,
     kRungaKutta4,
     kRungaKutta2,
     kInvalid,
@@ -27,10 +26,7 @@ enum class TimeDiscretizationMethod
 class TimeVariable
 {
   public:
-    TimeVariable()
-        : time_discretization_method_{TimeDiscretizationMethod::kInvalid}
-    {
-    }
+    TimeVariable() : time_discretization_method_{TimeDiscretizationMethod::kInvalid} {}
     TimeVariable(const SpatialVariable& u);
 
     ~TimeVariable() {}
@@ -44,23 +40,16 @@ class TimeVariable
           u_current_(other.u_current_),
           u_previous_(other.u_previous_){};
 
-    TimeVariable& operator=(const TimeVariable& other)
-    {
-        return *this = TimeVariable(other);
-    }
+    TimeVariable& operator=(const TimeVariable& other) { return *this = TimeVariable(other); }
     TimeVariable& operator=(TimeVariable&&) { return *this; }
 
   public:
-    void SetTimeDiscretizationMethod(
-        TimeDiscretizationMethod time_discretization_method);
+    void SetTimeDiscretizationMethod(TimeDiscretizationMethod time_discretization_method);
     TimeDiscretizationMethod GetTimeDiscretizationMethod() const;
     void SetStartTime(const double& start_time) { start_time_ = start_time; };
     void SetEndTime(const double& end_time) { end_time_ = end_time; };
     void SetTimeStep(const double& delta_t) { delta_t_ = delta_t; };
-    void SetInitialCondition(const std::vector<double>& u_initial)
-    {
-        u_previous_ = u_initial;
-    };
+    void SetInitialCondition(const std::vector<double>& u_initial) { u_previous_ = u_initial; };
     void Step(const std::vector<double>& wave_speeds);
 
   private:
